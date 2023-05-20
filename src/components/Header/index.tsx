5fd533eef31e73281a2228eb3abd0ce4fd7f62a9
ThyagoHSR <thyagohsr@gmail.com>
2023-05-20 19:24:27 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+import { HeaderButton, HeaderButtonsContainer, HeaderContainer } from './styles'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import coffeLogoImage from '../../assets/coffe-delivery-logo.svg'
+import { NavLink } from 'react-router-dom'
+import { useCart } from '../../hooks/useCart'
+
+export function Header() {
+  const { cartQuantity } = useCart()
+
+  return (
+    <HeaderContainer>
+      <div className="container">
+        <NavLink to="/">
+          <img src={coffeLogoImage} alt="" />
+        </NavLink>
+
+        <HeaderButtonsContainer>
+          <HeaderButton variant="purple">
+            <MapPin size={20} weight="fill" />
+            Recife-PE
+          </HeaderButton>
+          <NavLink to="/completeOrder">
+            <HeaderButton variant="yellow">
+              {cartQuantity >= 1 && <span>{cartQuantity}</span>}
+              <ShoppingCart size={20} weight="fill" />
+            </HeaderButton>
+          </NavLink>
+        </HeaderButtonsContainer>
+      </div>
+    </HeaderContainer>
+  )
+}
